f58b9d338935add6e78e30c274bd06fbb399ee7d
Vladimir <privezenoff@yandex.ru>
2020-02-20 18:18:39 +0300

    Update task_three.py

--- Lesson7/task_three.py
+++ Lesson7/task_three.py
@@ -8,22 +8,22 @@ from inspect import signature
 
 def curry(function):
     """Декоратор, выполняющий каррирование функции"""
-    number_of_arguments = (len(str(signature(function)).split(", ")))
+    number_of_arguments = len(signature(function).parameters)
 
     def inner_decorator(recursion_count, args):
-
         def actual_inner_decorator(arg):
             if recursion_count <= 1:
                 return function(*args, arg)
             args.append(arg)
             return inner_decorator(recursion_count - 1, args)
+
         return actual_inner_decorator
 
     return inner_decorator(number_of_arguments, [])
 
 
 @curry
-def sum_two_args(arg1, arg2):
+def sum_two_args(arg1: int, arg2: int):
     """Функция вычисляет сумму двух аргументов."""
     return arg1 + arg2
 
